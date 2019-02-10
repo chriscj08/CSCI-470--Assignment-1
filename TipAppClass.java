c7f9971ce8f529112892ca575ed50bd3b59b72d0
Chris jurrens <crjurrens@gmail.com>
2019-02-10 14:02:24 -0600

    Mostly done

--- TipAppClass.java
+++ TipAppClass.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TipAppClass
 {
-  TipCalculator calculator = new TipCalculator(); //Object used for calculating bill
+  public static TipCalculator calculator = new TipCalculator(); //Object used for calculating bill
   
   public static void main(String[] args)
   {
@@ -11,8 +11,11 @@ public class TipAppClass
   
   public static void calculateTips()
   {
+    System.out.print("***** Tip Calculator ***** \n\n\n"); 
     Scanner input = new Scanner(System.in);
     double billAmt = 0;
+    int tipPercent = 0;
+    int partySize = 0;
     boolean check = false;
     
     do //This do while loop makes sure the user enters a valid number. 
@@ -24,23 +27,80 @@ public class TipAppClass
         billAmt = input.nextDouble();
         
         if (billAmt < 0)
-          System.out.print("You cannot have a negative bill amount. \n");
+          System.out.print("You cannot have a negative bill amount. \n\n");
         else
         {
-          System.out.print("There you go! Your number is: ");
-          System.out.print(billAmt);
+          calculator.setBill(billAmt);
           check = true;
         }
       }
       catch(Exception e)
       {
         //If user enters something non-numeric, it will catch it.
-        System.out.print("Please enter a valid number. \n");
+        System.out.print("Please enter a valid bill amount. \n\n");
         input.next();
       }
     }while(check == false); //Loop ends when user enters a number greater than zero.
     
+    check = false; //Reset our test variable.
     
+    do //This do while loop makes sure the user enters a valid tip percentage. 
+    {
+      try
+      {
+        //Get user input.
+        System.out.print("Enter the tip percentage (20 is 20%): ");
+        tipPercent = input.nextInt();
+        
+        if (tipPercent < 0)
+          System.out.print("You cannot have a negative tip percentage. \n\n");
+        else
+        {
+          calculator.setTipPercentage(tipPercent);
+          check = true;
+        }
+      }
+      catch(Exception e)
+      {
+        //If user enters something non-numeric, it will catch it.
+        System.out.print("Please enter a valid tip percentage. \n\n");
+        input.next();
+      }
+    }while(check == false); //Loop ends when user enters a number greater than zero.
+    
+    check = false; //Reset our test variable
+    
+    do //This do while loop makes sure the user enters a valid tip percentage. 
+    {
+      try
+      {
+        //Get user input.
+        System.out.print("Enter the party size: ");
+        partySize = input.nextInt();
+        
+        if (partySize < 1)
+          System.out.print("You must have at least one person in your party. \n\n");
+        else
+        {
+          calculator.setPartySize(partySize);
+          check = true;
+        }
+      }
+      catch(Exception e)
+      {
+        //If user enters something non-numeric, it will catch it.
+        System.out.print("Please enter a valid party size. \n\n");
+        input.next();
+      }
+    }while(check == false); //Loop ends when user enters a number greater than zero.
+                     
+    //With everything we need, we can now calculate the bill and everyone's equal payment.
+                     
+    System.out.print("***** The Bill *****\n\n");
+                     
+    System.out.print("Bill amount: $" + calculator.getBill() + "\n");
+    System.out.print("Tip percent: " + calculator.getTipPercentage() + "%\n");
+    System.out.print("Party size: " + calculator.getPartySize() + "\n");                 
   }
 }
     
